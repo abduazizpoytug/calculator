dc866c9fda9db064ef944eec6da91edcfe6fa0ad
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:14:49 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,53 @@
+# boshlash = int(input('boshlash uchun 1ni bosing: '))
+# for x in range(123):
+#     print(121)
+
+
+
+# boshlash = input('boshlash uchun 1ni bosing: ').lo
+# while boshlash == '1':
+#     print('oyin ketti')
+#     print('oyin tugadi')
+#     boshlash = input('boshlash uchun 1ni bosing: ')
+
+
+
+
+
+
+# boshlash = input('boshlashni hohlaysizmi: ')
+# while boshlash == 'ha':
+#     num = int(input('sonni kiriting: '))
+#     num2 = int(input('ikkinchi sonni kiriting: '))
+#     value = input("amalni kiriting: ")
+#     if value == "+":
+#         print("natija: " + str(num + num2))
+#     elif value =="-":
+#         print("natija: " + str(num - num2))
+#     elif value =="*":
+#         print("natija: " + str(num * num2))
+#     elif value =="/":
+#         print("natija: " + str(num / num2))
+#     boshlash = input('qayta boshlash hohlaysizmi: ').lower()
+
+
+
+
+
+
+
+
+boshlash = input('boshlashni hohlaysizmi: ')
+while boshlash == 'ha':
+    num = int(input('sonni kiriting: '))
+    num2 = int(input('ikkinchi sonni kiriting: '))
+    value = input("amalni kiriting: ")
+    if value == "+":
+        print("natija: " + str(num + num2))
+    elif value =="-":
+        print("natija: " + str(num - num2))
+    elif value =="*":
+        print("natija: " + str(num * num2))
+    elif value =="/":
+        print("natija: " + str(num / num2))
+    boshlash = input('qayta boshlash hohlaysizmi: ').lower()
